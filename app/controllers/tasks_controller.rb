92bb5f44830f8402522e5b2aa35dc428b6a7a6d1
makotos3107 <sdf821m@gmail.com>
2021-06-21 21:34:47 +0900

    syusei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,9 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+
   def index
-    @user = User.find(session[:user_id])
-    @pagy,@tasks = pagy(@user.tasks, items: 3)
+    @pagy,@tasks = pagy(current_user.tasks, items: 3)
   end
 
   def show
@@ -48,10 +46,6 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
